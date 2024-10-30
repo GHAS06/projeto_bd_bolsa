ff18328a75c287f9d3270ca7b488de0ed6345810
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2024-10-30 09:34:47 -0300

    Atualizando o arquivo.zip de fevereiro para Março

--- Tira_duvida_dev/data_test.py
+++ Tira_duvida_dev/data_test.py
@@ -1,16 +1,17 @@
 import zipfile
 import pandas as pd 
+import psycopg2 
 
 # Caminho para o arquivo .zip
-caminho_zip = r'C:\Users\FABIA\Downloads\202402_NovoBolsaFamilia.zip'
+caminho_zip = r"C:\Users\FABIA\Downloads\202403_NovoBolsaFamilia.zip"
 
 # Lendo o arquivo .zip e listando todos os nomes de arquivos dentro dele
 with zipfile.ZipFile(caminho_zip, 'r') as meu_zip:  
     print(meu_zip.namelist())
-    
+
     # Lendo o arquivo CSV dentro do .zip com pandas
-    with meu_zip.open('202402_NovoBolsaFamilia.csv') as arquivo_csv:
+    with meu_zip.open('202403_NovoBolsaFamilia.csv') as arquivo_csv:
         # Tentando ler o CSV com uma codificação diferente
-        df = pd.read_csv(arquivo_csv, encoding='ISO-8859-1', nrows= 10000)  # ou 'latin1'
-        
-    print(df.head())  # Mostrar as 5 primeiros linhas 
+        df = pd.read_csv(arquivo_csv, encoding='ISO-8859-1', nrows= 1000)  # ou 'latin1'    
+    
+    print(df)
