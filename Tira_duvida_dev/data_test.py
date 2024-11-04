4a1352bda3366e0ae1e824dc08b041fde6f2fe1d
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2024-11-03 22:11:56 -0300

    Atualizações

--- Tira_duvida_dev/data_test.py
+++ Tira_duvida_dev/data_test.py
@@ -8,11 +8,13 @@ caminho_zip = r"C:\Users\FABIA\Downloads\202403_NovoBolsaFamilia.zip"
 # Lendo o arquivo .zip e listando todos os nomes de arquivos dentro dele
 with zipfile.ZipFile(caminho_zip, 'r') as meu_zip:  
     print(meu_zip.namelist())
+    print(meu_zip.infolist())
 
     # Lendo o arquivo CSV dentro do .zip com pandas
     with meu_zip.open('202403_NovoBolsaFamilia.csv') as arquivo_csv:
         # Tentando ler o CSV com uma codificação diferente
         df = pd.read_csv(arquivo_csv, delimiter=';', encoding='ISO-8859-1', nrows= 1000)  # ou 'latin1'    
     
-    #print(df.head()) # vai retornar as 5 primeiras linhas 
-    print (df.info()) 
+    print(df.head()) # vai retornar as 5 primeiras linhas 
+    
+    print(df.info()) #retornar as colunas e seus tipos de dados
